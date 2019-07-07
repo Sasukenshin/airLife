09c3b2728f5835ce38ea1dc124ea674f7025c9b7
Jeremy <Jeremy@192.168.1.14>
2019-07-07 20:46:28 +0200

    Deloremipsum de info-air et info-capteur

--- storage/framework/views/d2e2bb28478989847cde59cde5cd0ff8dac9ef6f.php
+++ storage/framework/views/d2e2bb28478989847cde59cde5cd0ff8dac9ef6f.php
@@ -8,40 +8,77 @@
             <div class="container-fluid dashboard-content">
                     <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
                         
-                        <h3 class="text-center">Informations générales sur l'air</h3>
+                        <h2 class="text-center">Informations générales sur l'air</h2>
                         
-                        <p>Ardeo, mihi credite, Patres conscripti (id quod vosmet de me existimatis et facitis ipsi) incredibili quodam amore patriae, qui me amor et subvenire
-                           olim impendentibus periculis maximis cum dimicatione capitis, et rursum, cum omnia tela undique esse intenta in patriam viderem, subire coegit atque
-                           excipere unum pro universis. Hic me meus in rem publicam animus pristinus ac perennis cum C. Caesare reducit, reconciliat, restituit in gratiam.</p>
-
-                        <p>Et est admodum mirum videre plebem innumeram mentibus ardore quodam infuso cum dimicationum curulium eventu pendentem. 
-                           haec similiaque memorabile nihil vel serium agi Romae permittunt. ergo redeundum ad textum.</p>
-
-                        <p>Haec igitur prima lex amicitiae sanciatur, ut ab amicis honesta petamus, amicorum causa honesta faciamus, ne exspectemus quidem,
-                           dum rogemur; studium semper adsit, cunctatio absit; consilium vero dare audeamus libere. Plurimum in amicitia amicorum bene suadentium
-                           valeat auctoritas, eaque et adhibeatur ad monendum non modo aperte sed etiam acriter, si res postulabit, et adhibitae pareatur.</p>
-
-                        <p>Haec et huius modi quaedam innumerabilia ultrix facinorum impiorum bonorumque praemiatrix aliquotiens operatur Adrastia atque utinam semper
-                           quam vocabulo duplici etiam Nemesim appellamus: ius quoddam sublime numinis efficacis, humanarum mentium opinione lunari circulo superpositum,
-                           vel ut definiunt alii, substantialis tutela generali potentia partilibus praesidens fatis, quam theologi veteres fingentes Iustitiae filiam ex abdita
-                           quadam aeternitate tradunt omnia despectare terrena.</p>
-
-                        <p>Horum adventum praedocti speculationibus fidis rectores militum tessera data sollemni armatos omnes celeri eduxere procursu et agiliter praeterito
-                           Calycadni fluminis ponte, cuius undarum magnitudo murorum adluit turres, in speciem locavere pugnandi. neque tamen exiluit quisquam nec permissus
-                           est congredi. formidabatur enim flagrans vesania manus et superior numero et ruitura sine respectu salutis in ferrum.</p>
+                        <h3>Dioxyde de Carbone (CO2)</h3>
+                        <h4>Caractéristiques</h4>
+                        <ul>
+                            <li>Incolore</li>
+                            <li>Inodore</li>
+                            <li>Ininflammable</li>
+                        </ul>
                         
-                        <p>Haec igitur prima lex amicitiae sanciatur, ut ab amicis honesta petamus, amicorum causa honesta faciamus, ne exspectemus quidem,
-                           dum rogemur; studium semper adsit, cunctatio absit; consilium vero dare audeamus libere. Plurimum in amicitia amicorum bene suadentium
-                           valeat auctoritas, eaque et adhibeatur ad monendum non modo aperte sed etiam acriter, si res postulabit, et adhibitae pareatur.</p>
+                        <h4>Dangers</h4>
 
-                        <p>Haec et huius modi quaedam innumerabilia ultrix facinorum impiorum bonorumque praemiatrix aliquotiens operatur Adrastia atque utinam semper
-                           quam vocabulo duplici etiam Nemesim appellamus: ius quoddam sublime numinis efficacis, humanarum mentium opinione lunari circulo superpositum,
-                           vel ut definiunt alii, substantialis tutela generali potentia partilibus praesidens fatis, quam theologi veteres fingentes Iustitiae filiam ex abdita
-                           quadam aeternitate tradunt omnia despectare terrena.</p>
+                        <p align="justify">Ce gaz respiré en trop grande quantité entraine successivement, une augmentation de l’amplitude respiratoire, une augmentation de la fréquence respiratoire, des troubles visuel, une perte de connaissance et un arrêt respiratoire pouvant aller jusqu’à une mort par asphyxie. Lorsqu’il est présent dans l’air ambiant il fait partie des rares gaz dangereux le plus lourd il se trouveras donc souvent près du sol.</p>
 
-                        <p>Horum adventum praedocti speculationibus fidis rectores militum tessera data sollemni armatos omnes celeri eduxere procursu et agiliter praeterito
-                           Calycadni fluminis ponte, cuius undarum magnitudo murorum adluit turres, in speciem locavere pugnandi. neque tamen exiluit quisquam nec permissus
-                           est congredi. formidabatur enim flagrans vesania manus et superior numero et ruitura sine respectu salutis in ferrum.</p>
+                        <h4>Informations générales</h4>
+                        
+                        <p align="justify">L’air ambiant contient environ 0,04% de Dioxyde de Carbone, a partir d’un tôt de 1,5% des effets cités ci-dessus peuvent se faire ressentir.</p>
+                        
+                        <h4>Utilité ou fun fact</h4>
+                        
+                        <p align="justify">On retrouve le CO2 dans les boissons gazeuses il est le gaz permettant de créer les bulles. 
+                           Il permet une meilleure croissance des plantes sous-marines.
+                           Le CO2 est un gaz qui à fort pourcentage de présence dans l’air permet d’éteindre les feux, certaines salles de serveur sont équipées de dispositif déversant du CO2 en cas d’incendie détecté.</p>
+
+                        <h3>Monoxyde de Carbone (CO)</h3>
+                        <h4>Caractéristiques</h4>
+                        <ul>
+                            <li>Incolore</li>
+                            <li>Inodore</li>
+                            <li>Inflammable</li>
+                        </ul>
+                        
+                        <h4>Dangers</h4>
+
+                        <p align="justify"> Le Monoxyde de Carbone est l’une des premières causes de mort toxique accidentelle ! Les estimation convergent vers 6000 mille mort par ans.  La plupart des morts sont faites par asphyxie étant données que les seuls symptômes qui est visible avant l’asphyxie ce sont des maux de tête pouvant s’apparenter à une migraine. On récence cependant quelques cas de brûlure à plusieurs degrés.</p>
+
+                        <h4>Informations générales</h4>
+                        
+                        <p align="justify">Le monoxyde de Carbone est présent notamment à cause des voitures qui en relâche une bonne quantité dans l’atmosphère mais il se forme lors des combustions partielles ou les combustions à très hautes température.</p>
+                        
+                        <h4>Utilité ou fun fact</h4>
+                        
+                        <p align="justify">Le CO a été utilisé pour les génocides lors de la seconde guerre mondiale.</p>
+                        
+                        
+                        <h3>Oxyde d’azote</h3>
+                        
+                        <h4>Dangers</h4>
+
+                        <p align="justify">Les Oxyde d’azote est le terme générique qui englobe un groupe de gaz hautement réactifs, tous contenant de l’azote et de l’oxygène dans des quantités différentes. De ce fait leur nocivité dépend énormément du gaz auxquels on se réfèrent mais dans l’ensemble les Oxydes d’azote sont 40 fois plus toxique que le Monoxyde de Carbone.</p>
+
+                        <h4>Informations générales</h4>
+                        
+                        <p align="justify">Les Oxyde d’azote interviennent notamment indirectement dans l’effet de serre. Dans un cas de plus forte concentration les Oxyde d’azote provoquent également des pluies acides.</p>
+                        
+                        <h3>Radon</h3>
+                        <h4>Caractéristiques</h4>
+                        <ul>
+                            <li>Incolore</li>
+                            <li>Inodore</li>
+                            <li>Radioactif</li>
+                        </ul>
+                        
+                        <h4>Dangers</h4>
+
+                        <p align="justify">Le Radon est un gaz qui est dangereux surtout sur la durée car il augmenter le risque de développer un cancer du poumon. Cette augmentation est proportionnelle à l’exposition cumulée tout au long de sa vie.</p>
+
+                        <h4>Informations générales</h4>
+                        
+                        <p align="justify">Ce gaz se retrouve bien sûr en plus grande quantité près des sites ou il y a une centrale nucléaire il est également beaucoup plus important de surveiller ce taux.</p>
+
                         
                     </div>
             </div>
